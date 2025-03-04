23fdcc45448cd31ee9c65aa05da7158b97b517c1
GuidoCanso <guidocansobre@gmail.com>
2025-03-04 19:02:03 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let palos = ["♦", "♥", "♠ ", "♣"];
+
+let randomCardNumber = Math.floor(Math.random() * cardNumber.length)
+let randomPalosNumber = Math.floor(Math.random() * palos.length)
+
+let randomCard = cardNumber[randomCardNumber]
+let randomPalos = palos[randomPalosNumber]
+
+let paloSup = document.getElementById("paloSuperior")
+let numero = document.getElementById("numero")
+let paloInf = document.getElementById("paloInferior")
+
+paloSup.innerHTML = randomPalos
+numero.innerHTML = randomCard
+paloInf.innerHTML = randomPalos
+
+if (randomPalos == "♦" || randomPalos == "♥") {
+  paloSup.classList.add("rojo")
+  paloInf.classList.add("rojo")
+}
+}
+
+
